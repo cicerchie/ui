1fe036bebc932eeef0fda8c98b4f049275b30fe5
cicerchia <70173987+cicerchia@users.noreply.github.com>
2020-08-31 17:58:17 +0200

    fix: add WAI-ARIA role to Spinner

--- src/loading/Spinner.svelte
+++ src/loading/Spinner.svelte
@@ -7,4 +7,5 @@
 
 <div
   class={`border-current rounded-full animate-spin ${sm ? 'w-4 h-4 border-2' : 'w-8 h-8 border-4'}${className ? ` ${className}` : ''}`}
-  style="border-right-color: transparent" />
+  style="border-right-color: transparent"
+  role="status" />
