1699526eebe43bb8ef2b7458b2864366e39728ec
cicerchia <70173987+cicerchia@users.noreply.github.com>
2020-08-31 19:18:48 +0200

    fix: add sr-only message and aria-hidden to Spinner

--- src/loading/Spinner.svelte
+++ src/loading/Spinner.svelte
@@ -2,10 +2,16 @@
   let className = null
   export { className as class }
 
+  export let aria = {}
+  export let srOnlyMessage = 'Loading...'
+  
   export let sm = null
 </script>
 
 <div
   class={`border-current rounded-full animate-spin ${sm ? 'w-4 h-4 border-2' : 'w-8 h-8 border-4'}${className ? ` ${className}` : ''}`}
   style="border-right-color: transparent"
-  role="status" />
+  role="status"
+  aria-hidden={aria.hidden}>
+  {#if srOnlyMessage}<span class="sr-only">{srOnlyMessage}</span>{/if}
+</div>
