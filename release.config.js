528a49db1845cd5de9d034c3cfa859f947137ad8
cicerchia <70173987+cicerchia@users.noreply.github.com>
2021-08-19 17:52:46 +0200

    ci: publish to npm from "package" dir

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/github",
-    "@semantic-release/npm",
+    ["@semantic-release/npm", { pkgRoot: "package" }],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/git",
