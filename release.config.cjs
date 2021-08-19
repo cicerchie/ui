34fdcf33a4d06a3b43ee90124f64bcf27025823b
cicerchia <70173987+cicerchia@users.noreply.github.com>
2021-08-19 18:53:29 +0200

    ci: fix semantic-release npm publish

--- release.config.cjs
+++ release.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
-    "@semantic-release/github",
-    ["@semantic-release/npm", { pkgRoot: "./package" }],
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
+    "@semantic-release/github",
+    ["@semantic-release/npm", { pkgRoot: "./package" }],
     "@semantic-release/git",
   ],
 };
