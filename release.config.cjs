87b0158a512b82122b57d509edebd9b91585eb87
cicerchia <70173987+cicerchia@users.noreply.github.com>
2023-04-08 18:58:58 +0200

    chore: do not use package as dir for semantic-release npm plugin anymore

--- release.config.cjs
+++ release.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/github",
-    ["@semantic-release/npm", { pkgRoot: "./package" }],
+    "@semantic-release/npm",
     "@semantic-release/git",
   ],
 };
